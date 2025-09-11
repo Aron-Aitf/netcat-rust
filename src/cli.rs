19cc98c955b35aab8d2cfa478f0a7af79e99d841
Aron Atif <aronatif@360gmail.com>
2025-09-11 22:33:03 +0530

    fixed read,

--- src/cli.rs
+++ src/cli.rs
@@ -5,8 +5,10 @@ use clap::Parser;
 #[derive(Parser, Debug, Clone)]
 pub struct CliArgs {
     pub root_url: String,
-    #[arg(short, long = "input-prompt", default_value_t = String::from_str(">").unwrap())]
+    #[arg(short, long = "input_prompt", default_value_t = String::from_str(">").unwrap())]
     pub input_prompt: String,
-    #[arg(short, long = "output-prompt", default_value_t = String::from_str("").unwrap())]
+    #[arg(short, long = "output_prompt", default_value_t = String::from_str("").unwrap())]
     pub output_prompt: String,
+    #[arg(short, long, default_value_t = 1024)]
+    pub buffer_size: u16
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod cli;
 use std::{
-    error::Error, io::{Read as _, Write as _, stdin, stdout}, net::TcpStream
+    error::Error,
+    io::{Read as _, Write as _, stdin, stdout},
+    net::TcpStream,
 };
 
 use clap::Parser;
 use cli::CliArgs;
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     let args = CliArgs::parse();
     let mut stream = TcpStream::connect(args.root_url)?;
     loop {
@@ -14,10 +16,12 @@ fn main() -> Result<(), Box<dyn Error>>{
         let mut user_input = String::new();
         stdin().read_line(&mut user_input)?;
         stream.write_all(user_input.as_bytes())?;
-        let mut url_output_buffer = Vec::new();
-        stream.read_to_end(&mut url_output_buffer)?;
+        let mut url_output_buffer = vec![0; args.buffer_size.into()];
+        #[allow(clippy::unused_io_amount)]
+        stream.read(&mut url_output_buffer)?;
         let url_output_buffer = String::from_utf8_lossy(&url_output_buffer);
-        println!("{}{}", args.output_prompt, url_output_buffer);
+        print!("{}{}", args.output_prompt, url_output_buffer);
+        stdout().flush()?;
     }
 }
-// del netcat.exe && move ./target/debug/netcat.exe netcat.exe
+// cargo build && del netcat.exe && move ./target/debug/netcat.exe netcat.exe
