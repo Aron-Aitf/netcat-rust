d86a7e91c5dbe0169e8ad5df56b3751ba68373f2
Aron Atif <aronatif@360gmail.com>
2025-09-11 22:07:22 +0530

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 mod cli;
 use std::{
-    io::{Read as _, Write as _, stdin, stdout},
-    net::TcpStream,
+    error::Error, io::{Read as _, Write as _, stdin, stdout}, net::TcpStream
 };
 
 use clap::Parser;
 use cli::CliArgs;
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     let args = CliArgs::parse();
-    let mut stream = TcpStream::connect(args.root_url).unwrap();
+    let mut stream = TcpStream::connect(args.root_url)?;
     loop {
         print!("{}", args.input_prompt);
-        stdout().flush().unwrap();
+        stdout().flush()?;
         let mut user_input = String::new();
-        stdin().read_line(&mut user_input).unwrap();
-        stream.write_all(user_input.as_bytes()).unwrap();
+        stdin().read_line(&mut user_input)?;
+        stream.write_all(user_input.as_bytes())?;
         let mut url_output_buffer = Vec::new();
-        stream.read_to_end(&mut url_output_buffer).unwrap();
+        stream.read_to_end(&mut url_output_buffer)?;
         let url_output_buffer = String::from_utf8_lossy(&url_output_buffer);
         println!("{}{}", args.output_prompt, url_output_buffer);
     }
