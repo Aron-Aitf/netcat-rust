d44f6103e914611e5c5d7ac3bcb761f5266a9970
Aron Atif <aronatif@360gmail.com>
2025-09-12 07:50:37 +0530

    added readme and license

---
+++ README.md
@@ -0,0 +1,2 @@
+# A clone of netcat in rust 
+## This was made because netcat does not work on windows
--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         stdin().read_line(&mut user_input)?;
 
         stream.write_all(user_input.as_bytes())?;
-        
+
         let mut url_output_buffer = vec![0; args.buffer_size.into()];
         #[allow(clippy::unused_io_amount)]
         stream.read(&mut url_output_buffer)?;
         let url_output_buffer = String::from_utf8_lossy(&url_output_buffer);
-        
+
         print!("{}{}", args.output_prompt, url_output_buffer);
         stdout().flush()?;
     }
